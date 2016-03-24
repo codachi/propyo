429aeb307da96c77de9fc3ee6419af11dfd0f8c8
peposso <pepo.pepo.peposso@gmail.com>
2016-03-24 22:54:39 +0900

    Update debugGuireflesh.js

--- propyo/Assets/Resources/script/common/debugGuireflesh.js
+++ propyo/Assets/Resources/script/common/debugGuireflesh.js
@@ -1,15 +1,13 @@
 ﻿#pragma strict
 
 
-static var reguiExt = function(num,contentAr:Array){//デバッグテキストの更新
-var ob:GUIText = (contentAr[4] as GameObject).transform.Find("debugText(Clone)").gameObject.GetComponent.<GUIText>();//オブジェクトから子を取り出し
+static var reguiExt = function(num, contentAr:Array) { // デバッグテキストの更新
+    // オブジェクトから子を取り出し
+    var ob:GUIText = (contentAr[4] as GameObject).transform.Find("debugText(Clone)").gameObject.GetComponent.<GUIText>();
 
-var sumtxt = num.ToString();//合体地
+    var sumtxt = num.ToString();  // 合体地
 
-
-//print("テキスト" + sumtxt);
-
-//print("オブ" + ob);
-ob.text = sumtxt;
-
+    // print("テキスト" + sumtxt);
+    // print("オブ" + ob);
+    ob.text = sumtxt;
 };
